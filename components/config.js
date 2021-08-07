3d410f7539daa68e6c8cc3b54e7b31b7cef3ec5a
Fokion Sotiropoulos <fokion.s@gmail.com>
2021-08-07 12:46:29 +0100

    endpoint url

--- components/config.js
+++ components/config.js
@@ -1,4 +1,4 @@
 
 import path from 'path'
-export const server = process.env.NODE_ENV !== 'production'? "http://localhost:3000":"";
+export const server = process.env.ENDPOINT_URL? process.env.ENDPOINT_URL: "http://localhost:3000";
 export const endpointsLocation = process.env.ENDPOINTS ? process.env.ENDPOINTS : path.join(process.cwd(),"endpoints.json")
